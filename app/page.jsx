9116be00572cdf2e55a993c8a900b082f6eb82b0
WezeniteAdmin <system@wezenite.com>
2024-05-07 19:17:07 +0530

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1> HomePage </h1>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos repellat
+        tempore laboriosam sapiente harum doloremque suscipit. Aperiam,
+        praesentium? Id nostrum necessitatibus quidem minus aliquam eveniet odio
+        totam repellat laudantium facere molestiae, aliquid eos earum. Ipsam
+        corporis, officiis excepturi consectetur vel voluptatibus quae,
+        recusandae harum eos explicabo laborum doloribus. Minima, quos.{' '}
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
